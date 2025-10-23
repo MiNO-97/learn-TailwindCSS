4151b600d102c814679a94e289ea8f942c527dd2
Mino Thavisack <phanthavong310@gmail.com>
2025-10-24 00:04:05 +0700

    clone project shopme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        lao: ['"Noto Serif Lao"', 'sans-serif'],
+        sans: ['"Noto Serif Lao"', 'sans-serif'],
       },
     },
   },
